df3261658011d2c80db323c9fc9dd58f7ffa9cd7
Vlad Munteanu <paulvlad34@gmail.com>
2024-03-30 14:26:24 +0200

    fix: color

--- src/components/UserProfilePage.tsx
+++ src/components/UserProfilePage.tsx
@@ -6,7 +6,7 @@ import {
   Image,
   Link,
   Text,
-  VStack
+  VStack,
 } from "@chakra-ui/react";
 import { FaEnvelope, FaPhone } from "react-icons/fa";
 import { useSearchParams } from "react-router-dom";
@@ -15,26 +15,31 @@ import MapButton from "./MapButton";
 
 const UserProfilePage = () => {
   const [searchParams] = useSearchParams();
-  console.log(searchParams)
+  console.log(searchParams);
   const firstName = searchParams.get("firstName");
   const lastName = searchParams.get("lastName");
   const mobilePhone = searchParams.get("mobilePhone");
   const email = searchParams.get("email");
   return (
-    <Box bg="yellow.400" w="full" p={4} color="white">
+    <Box bg="#EAC31F" w="full" p={4} color="white">
       <VStack spacing={4} align="stretch" marginBottom={10}>
         <Center>
           <Image
             borderRadius="full"
-            boxSize="100px"
-            src="https://i.imgur.com/4cHMqub.jpeg"
+            boxSize="150px"
+            src="https://i.imgur.com/YHnWS6n.png"
             alt="Profile image"
+            objectFit="contain"
+            backgroundColor={"white"}
+            p={2}
+            mb={3}
           />
         </Center>
         <Box textAlign="center">
-          <Text fontSize="xl" fontWeight="bold">
+          <Text fontSize="xl" fontWeight="bold" mb={1}>
             {`${firstName} ${lastName}`}
           </Text>
+          <Text fontSize="md">Crystal Logistics Services</Text>
           <Text fontSize="md">Freight Forwarder</Text>
         </Box>
         <HStack justify="center" spacing={10}>
