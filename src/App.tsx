e39b8828c2d5824ef043b214abdddccf30fc8413
Vlad Munteanu <paulvlad34@gmail.com>
2024-04-03 11:04:13 +0300

    feat: update employees

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import {
-  ChakraProvider,
-  theme
-} from "@chakra-ui/react";
-import {
-  Route,
-  BrowserRouter as Router,
-  Routes
-} from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import UserProfile from "./components/UserProfile";
 import "./index.css";
 
@@ -14,8 +7,7 @@ export const App = () => (
   <ChakraProvider>
     <Router>
       <Routes>
-        <Route path="/*" Component={UserProfile}>
-        </Route>
+        <Route path="/*" Component={UserProfile}></Route>
         {/* Other routes here */}
       </Routes>
     </Router>
